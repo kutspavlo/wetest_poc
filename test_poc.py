e4e55eb77e81d2a4c6f051d4b0d96f2145dd395d
kutspavlo <kutspavlo28@gmail.com>
2025-11-06 00:42:44 +0200

    add anchor element

--- test_poc.py
+++ test_poc.py
@@ -5,16 +5,16 @@ from poco.drivers.cocosjs import CocosJsPoco
 
 
 def test_simple_login_button_click():
-    connect_device("Android:///")
+    # connect_device("Android:///")
     poco = CocosJsPoco()
 
+    mtt_button = poco("HallScene").offspring("mtt")
+    mtt_button.wait_for_appearance(timeout=30)
+    time.sleep(5)
     login_button = poco("HallScene").offspring("btn_login")
+    login_button.click()
 
-    login_button.wait_for_appearance(timeout=120)
-    time.sleep(30)
-    # login_button.click()
-    #
-    # login_node = poco("HallScene").offspring("LoginNode")
-    # login_node.wait_for_appearance(timeout=120)
-    #
-    # assert login_node.exists(), "Login failed: Did not find 'Login Node' element."
+    login_node = poco("HallScene").offspring("LoginNode")
+    login_node.wait_for_appearance(timeout=120)
+
+    assert login_node.exists(), "Login failed: Did not find 'Login Node' element."
