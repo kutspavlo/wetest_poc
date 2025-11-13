11e79838ff4aa996debaa6de02116cf60d0ea03c
pavlo_kuts <pavlo.kuts@a5labs.co>
2025-11-13 18:03:52 +0200

    print test function

--- runTest.sh
+++ runTest.sh
@@ -10,6 +10,7 @@ pip3 install -r requirements.txt
 
 echo "--- 3. Running Pytest (using python3 -m) ---"
 echo $CASE_NAME
+echo $CASE_FUNC
 TEST_PATH="tests/${CASE_NAME}"
 python3 -m pytest --capture=no "$TEST_PATH"
 
