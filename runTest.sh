4591e938b9366ee9f3ad9b75c7f3996fd0dc41d8
pavlo_kuts <pavlo.kuts@a5labs.co>
2025-11-13 16:27:43 +0200

    remove run trigger

--- runTest.sh
+++ runTest.sh
@@ -8,7 +8,7 @@ echo "Using system Python 3..."
 echo "--- 2. Installing Dependencies (using pip3) ---"
 pip3 install -r requirements.txt
 
-echo "--- 3. Running Pytest (using python3 -m) ---"
-python3 -m pytest
+#echo "--- 3. Running Pytest (using python3 -m) ---"
+#python3 -m pytest
 
 echo "--- Test execution finished ---"
