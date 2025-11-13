f448f4bd57dc2a5d2a73869d2cc42529722e0fd9
pavlo_kuts <pavlo.kuts@a5labs.co>
2025-11-13 17:16:28 +0200

    update run trigger - consider tests folder (fix)

--- runTest.sh
+++ runTest.sh
@@ -10,7 +10,7 @@ pip3 install -r requirements.txt
 
 echo "--- 3. Running Pytest (using python3 -m) ---"
 echo $CASE_NAME
-TEST_PATH="tests/${CASE_NAME}::${CASE_FUNC}"
+TEST_PATH="tests/${CASE_NAME}"
 python3 -m pytest --capture=no "$TEST_PATH"
 
 echo "--- Test execution finished ---"
