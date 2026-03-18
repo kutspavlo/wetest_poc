406956c80f9899d659564e2acf53b1def07f06f5
pavlo_kuts <pavlo.kuts@a5labs.co>
2026-03-18 22:21:44 +0200

    fix runTest typo

--- runTest.sh
+++ runTest.sh
@@ -61,8 +61,8 @@ if [ "$REPORT_FLAG" == "true" ]; then
             --project-id 7 \
             --name "$CASE_FUNC-($(date +'%Y/%m/%d %H:%M'))" \
             --source "WeTest" \
-            --results results.xml
-            --property-map response_body:note domain_url:field:domain_url
+            --results results.xml \
+            --property-map domain_url:field:domain_url status_code:field:status_code response_body:note
 
         UPLOAD_STATUS=$?
         if [ $UPLOAD_STATUS -ne 0 ]; then
